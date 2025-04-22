ac94d601848e6df54f2c66f6b4f770fcbf468efc
Hafez <a.hafez852@gmail.com>
2025-04-22 23:46:31 +0200

    feat: add sqs to lambda integration

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,4 @@
-module "lambda_function" {
+module "generate_random_person_lambda" {
   source = "terraform-aws-modules/lambda/aws"
 
   function_name = "generate-random-person"
@@ -6,5 +6,37 @@ module "lambda_function" {
   handler       = "index.handler"
   runtime       = "nodejs22.x"
 
-  source_path = "./placeholder-lambda/index.js"
+  source_path = "${path.module}/placeholder-lambda"
+  publish     = true
+
+  attach_policy_statements = true
+  policy_statements = [
+    {
+      actions   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"]
+      resources = [module.generate_random_person_sqs.queue_arn]
+    }
+  ]
+
+}
+
+module "generate_random_person_sqs" {
+  source = "terraform-aws-modules/sqs/aws"
+
+  name = "generate-random-person-queue"
+
+  create_dlq                      = true
+  create_dlq_redrive_allow_policy = true
+  fifo_queue                      = true
+  content_based_deduplication     = true
+
+  visibility_timeout_seconds = 10
+  redrive_policy = {
+    maxReceiveCount = 3
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
+  event_source_arn = module.generate_random_person_sqs.queue_arn
+  function_name    = module.generate_random_person_lambda.lambda_function_arn
+  batch_size       = 1
 }
